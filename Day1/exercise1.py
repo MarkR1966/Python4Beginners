9e09543dea890a6175526327a55336a891959fab
MarkR1966 <markrafferty27@gmail.com>
2020-06-29 16:08:14 +0100

    Initial commit

---
+++ Day1/exercise1.py
@@ -0,0 +1,16 @@
+print       ("Hello World")
+# PRINT "Hello World"     Command Capitalised, No brackets
+print ("Hello World")
+# print(Hello World)      No quotes around txt
+# Print("Hello World")    Command should not have Capital
+# priint("Hello World")   Spelling mistake in command
+
+fname=input('Please enter your first name:')
+lname=input('Please enter your last name:')
+print('Hello ',fname,lname)
+
+number1 = float(input("Please eneter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
